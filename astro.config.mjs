08fea91f4dc22488e0fb6e846fbb027ba929777c
Michael V. Colianna <michael.colianna@me.com>
2024-07-23 23:13:36 -0400

    Gets the skeleton for Storyblok elements and styling in - Adds basicSsl for Storyblok preview - Installs Tailwind/Vite - Adds tsconfig options - Sets up the templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,42 @@
 import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro'
+import tailwind from '@astrojs/tailwind'
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.MODE === 'production' ? 'https://mvc.codes' : 'http://localhost:4321',
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        card: 'components/storyblok/Card',
+        cardSection: 'components/storyblok/CardSection',
+        experienceItem: 'components/storyblok/ExperienceItem',
+        experienceSection: 'components/storyblok/ExperienceSection',
+        hero: 'components/storyblok/Hero',
+        page: 'components/storyblok/Page',
+        portfolioItem: 'components/storyblok/PortfolioItem',
+        portfolioSection: 'components/storyblok/PortfolioSection',
+        simpleHeader: 'components/storyblok/SimpleHeader',
+      },
+      apiOptions: {
+        region: 'us',
+      },
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  site: import.meta.env.MODE === 'production' ? 'https://mvc.codes' : 'https://localhost:4321',
+  vite: {
+    plugins: [
+      basicSsl(),
+    ],
+    server: {
+      https: true,
+    },
+  },
 })
